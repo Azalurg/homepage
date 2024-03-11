c9e32ea569e7777a479a6bf0de356a1c7882506a
Azalurg <91213389+Azalurg@users.noreply.github.com%>
2024-03-11 09:03:49 +0100

    Add moving average indicator to crypto prices

--- script/index.js
+++ script/index.js
@@ -1,36 +1,49 @@
 import config from "../data/config.json" assert {type: 'json'}
 
+function get_avg(prices) {
+    if (prices.length > config.movingAvgPeriod) {
+        prices.shift()
+    }
+    let s = prices.reduce((a, b) => parseFloat(a) + parseFloat(b))
+    return s / prices.length;
+}
+
 // Crypto price
 const foreground = '#d8dee9'
 const red = '#e06c75'
 const green = '#98c379'
 
-let ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@kline_5m");
-let stockPriceElement = document.getElementById("btc-price");
-let lastPrice = null;
+let ws_btc = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@kline_5m");
+let ws_eth = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@kline_5m");
 
-ws.onmessage = (event) => {
-    let stockObject = JSON.parse(event.data);
-    let price = parseFloat(stockObject.k.c).toFixed(2);
-    stockPriceElement.innerText = "$" + price;
-    stockPriceElement.style.color = !lastPrice || lastPrice === price ? foreground : price > lastPrice ? green : red
-    lastPrice = price;
+let element_btc = document.getElementById("btc-price");
+let element_eth = document.getElementById("eth-price");
+let prices_btc = []
+let prices_eth = []
+
+ws_btc.onmessage = (event) => {
+    let data_json = JSON.parse(event.data);
+    let price = parseFloat(data_json.k.c).toFixed(2);
+    prices_btc.push(price)
+
+    let avg_price = get_avg(prices_btc)
+    element_btc.innerText = "$" + price;
+    element_btc.style.color = avg_price === price ? foreground : price > avg_price ? green : red
 }
 
-let ws_eth = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@kline_5m");
-let stockPriceElement_eth = document.getElementById("eth-price");
-let lastPrice_eth = null;
-
 ws_eth.onmessage = (event) => {
-    let stockObject = JSON.parse(event.data);
-    let price = parseFloat(stockObject.k.c).toFixed(2);
-    stockPriceElement_eth.innerText = "$" + price;
-    stockPriceElement_eth.style.color = !lastPrice_eth || lastPrice_eth === price ? foreground : price > lastPrice_eth ? green : red
-    lastPrice_eth = price;
+    let data_json = JSON.parse(event.data);
+    let price = parseFloat(data_json.k.c).toFixed(2);
+    prices_eth.push(price)
+
+    let avg_price = get_avg(prices_eth)
+    element_eth.innerText = "$" + price;
+    element_eth.style.color = avg_price === price ? foreground : price > avg_price ? green : red
+
 }
 
 // Time
-setInterval(()=> {
+setInterval(() => {
     const current_date = new Date();
     time.innerText = current_date.toLocaleTimeString('pl-PL');
 }, 1000)
@@ -39,7 +52,7 @@ setInterval(()=> {
 date.innerText = new Date().toLocaleDateString('pl-PL');
 
 // Background
-if (config.dynamicBackground){
+if (config.dynamicBackground) {
     body.style.backgroundImage = `url(https://picsum.photos/1920/1080?random=1)`;
 }
 
@@ -47,4 +60,4 @@ if (config.dynamicBackground){
 document.querySelector('.js-form')?.addEventListener('submit', e => {
     e.preventDefault();
     window.open(`https://duckduckgo.com/?q=${searchValue.value}`, "_self")
-  });
+});
